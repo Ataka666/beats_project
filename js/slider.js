9559adf5d5a184c4808cbcad997687ecefcdb488
Anastasia Trofimova <atakaataka666@gmail.com>
2020-09-17 20:41:34 +0700

    ask week 4

--- js/slider.js
+++ js/slider.js
@@ -1,14 +1,17 @@
 const cross = document.querySelector("#cross");
 const overlay = document.querySelector("#overlay");
-  const hamburger = document.querySelector("#hamburger");
-  const computed = window.getComputedStyle(overlay);
+const hamburger = document.querySelector("#hamburger");
+const computed = window.getComputedStyle(overlay);
 
-  hamburger.addEventListener("click", function(e) {
-    e.preventDefault();
-    overlay.style.right = "0px";
-  });
+hamburger.addEventListener("click", function (e) {
+  e.preventDefault();
+  overlay.style.right = "0px";
+});
 
-  cross.addEventListener("click", function(e) {
-    e.preventDefault();
-    overlay.style.right = "-1000px";
-  });
+cross.addEventListener("click", function (e) {
+  e.preventDefault();
+  overlay.style.right = "-1000px";
+});
+overlay.addEventListener("click", function (e) {
+  overlay.style.right = "-1000px";
+});
