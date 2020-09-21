b3c2505f48001e7e34c3ac8c31926e59c9c7447f
Anastasia Trofimova <atakaataka666@gmail.com>
2020-09-21 23:25:41 +0700

    week4

--- js/form.js
+++ js/form.js
@@ -1,46 +1,48 @@
-const form = document.querySelector('.form');
-const button = document.querySelector('#myButton');
-button.addEventListener('click', ()=>{
-    function validateForm(form) {
-        let valid = true;
+// const form = document.querySelector('.form');
+// const button = document.querySelector('#myButton');
+// button.addEventListener('click', ()=>{
+//     function validateForm(form) {
+//         let valid = true;
 
-        if (!validateField(form.elements.name)) {
-            valid = false;
-        }
+//         if (!validateField(form.elements.name)) {
+//             valid = false;
+//         }
 
-        if (!validateField(form.elements.lastName)) {
-            valid = false;
-        }
+//         if (!validateField(form.elements.lastName)) {
+//             valid = false;
+//         }
 
-        if (!validateField(form.elements.comment)) {
-            valid = false;
-        }
+//         if (!validateField(form.elements.comment)) {
+//             valid = false;
+//         }
 
-        return valid;
-    }
+//         return valid;
+//     };
 
-    function validateField(field) {
-        if (!field.checkValidity()) {
-            field.nextElementSibling.textContent = field.validationMessage;
+//     function validateField(field) {
+//         if (!field.checkValidity()) {
+//             field.nextElementSibling.textContent = field.validationMessage;
 
-            return false;
-        } else {
-            field.nextElementSibling.textContent = '';
+//             return false;
+//         } else {
+//             field.nextElementSibling.textContent = '';
 
-            return true;
-        }
-    }
-    function validateField(field) {
-        field.nextElementSibling.textContent = field.validationMessage;
-        return field.checkValidity();
-    }
-if (validateForm(form)){
-    const order = {
-    name: form.elements.name.value,
-    lastName: form.elements.lastName.value,
-    comment: form.elements.comment.value}; 
-    const xhr=new XMLHttpsRequest();
-    xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail');
-    xhr.send(JSON.stringify(order));
-    xhr.addEventListener('load', () => {
-    console.log(xhr.response)})})
+//             return true;
+//         }
+//     };
+//     function validateField(field) {
+//         field.nextElementSibling.textContent = field.validationMessage;
+//         return field.checkValidity();
+//     };
+// if (validateForm(form)){
+//     const order = {
+//     name: form.elements.name.value,
+//     lastName: form.elements.lastName.value,
+//     comment: form.elements.comment.value
+// }; 
+//     const xhr=new XMLHttpsRequest();
+//     xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail');
+//     xhr.send(JSON.stringify(order));
+//     xhr.addEventListener('load', () => {
+//     console.log(xhr.response)})
+// }})
---
+++ js/modal.js
@@ -0,0 +1,57 @@
+const validateFields=(form, Array)=>{
+    Array.forEach((field) => {
+        field.removeClass('input--error');
+        
+        if (field.val().trim()==''){
+        field.addClass('input--error')}
+    })
+
+    const errorFields=form.find('.input--error');
+    return errorFields.length==0;
+}
+
+$(".form").submit(e=>{
+    e.preventDefault();
+    const form=$(e.currentTarget);
+    const name= form.find("[name='name']");
+    const phone=form.find("[name='phone']");
+    const comment=form.find("[name='comment']");
+    const to=form.find("[name='to']");
+    const isValid=validateFields(form, [name,phone,comment]);
+    const modal=$('#modal');
+    const content=modal.find(".modal__content");
+    modal.css('color', 'black');
+
+   if (isValid){ 
+    $.ajax({
+    url:"https://webdev-api.loftschool.com/sendmail",
+    method:"post",
+    data: {
+        name:name.val(),
+        phone:phone.val(),
+        comment:comment.val(),
+        to:to.val(),
+    },
+    success: data=>{
+        content.text(data.message);
+        $.fancybox.open({
+            src:"#modal",
+            class:"inline"
+        })
+    },
+    error: data=>{
+        const message= data.responseJSON.message;
+        content.text(message);
+        modal.css('color', 'red');
+        $.fancybox.open({
+            src:"#modal",
+            class:"inline"
+        })
+    }
+})
+   }
+})
+$(".app-close-modal").click(e=>{
+    e.preventDefault();
+    $.fancybox.close()
+})
